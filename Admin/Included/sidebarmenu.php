961d16f5102d9d9a869b34854f9d05c4b1b65006
James456 <np03cs4s210042@heraldcollege.edu.np>
2022-04-05 19:49:50 +0545

    Updated

--- Admin/Included/sidebarmenu.php
+++ Admin/Included/sidebarmenu.php
@@ -1,6 +1,6 @@
 <div class="sidebar-menu">
 					<header class="logo1">
-						<a href="#" class="sidebar-icon"> <span class="fa fa-book fa-2xl"></span> </a> 
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
 					</header>
 						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
                            <div class="menu">
