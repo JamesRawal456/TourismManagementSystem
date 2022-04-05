639f05aac275efdd74c4931ce8fdc1616f8e9921
James456 <np03cs4s210042@heraldcollege.edu.np>
2022-04-05 19:06:09 +0545

    Updated

--- Admin/Included/sidebarmenu.php
+++ Admin/Included/sidebarmenu.php
@@ -1,24 +1,23 @@
 <div class="sidebar-menu">
 					<header class="logo1">
-						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+						<a href="#" class="sidebar-icon"> <span class="fa fa-book fa-2xl"></span> </a> 
 					</header>
 						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
                            <div class="menu">
 									<ul id="menu" >
 										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
 										
-									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Tour Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
 										   <ul id="menu-academico-sub" >
-										   <li id="menu-academico-avaliacoes" ><a href="createpackage.php">Create</a></li>
+										   <li id="menu-academico-avaliacoes" ><a href="createpackage.php">add</a></li>
 											<li id="menu-academico-avaliacoes" ><a href="managepackages.php">Manage</a></li>
 										  </ul>
 										</li>
-									<li id="menu-academico" ><a href="manageuser.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage Users</span><div class="clearfix"></div></a></li>
+									<li id="menu-academico" ><a href="managebooking.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage Booking</span><div class="clearfix"></div></a></li>
 									
-									<li><a href="managebooking.php"><i class="fa fa-list" aria-hidden="true"></i>  <span>Manage Booking</span><div class="clearfix"></div></a></li>
+									<li><a href="manageuser.php"><i class="fa fa-list" aria-hidden="true"></i>  <span>Manage Users</span><div class="clearfix"></div></a></li>
 									 <li><a href="manageissues.php"><i class="fa fa-table"></i>  <span>Manage Issues</span><div class="clearfix"></div></a></li>
 									<li><a href="manageenquires.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Enquiries</span><div class="clearfix"></div></a></li>
-									<li><a href="manage-pages.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Pages</span><div class="clearfix"></div></a></li>
 							     
 									
 								  </ul>
