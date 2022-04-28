5f0ff57dbf6fb326cd35027ee5a88b991c4d019d
James456 <np03cs4s210042@heraldcollege.edu.np>
2022-04-28 17:15:03 +0545

    changes

--- Admin/dashboard2.php
+++ Admin/dashboard2.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link href="css/bootdash.css" rel='stylesheet' type='text/css' />
-    <link href="css/test.css" rel='stylesheet' type='text/css' /> 
-
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Dashboard</title>
+<link href="css/bootdash.css" rel='stylesheet' type='text/css' />
+<link href="css/workall.css" rel='stylesheet' type='text/css' /> 
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 </head>
 <body class="bg-content">
