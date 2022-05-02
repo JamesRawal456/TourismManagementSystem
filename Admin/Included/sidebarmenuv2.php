471c2babd9df3e7f523ec6898cb92ce48f6e8380
James456 <np03cs4s210042@heraldcollege.edu.np>
2022-05-02 22:04:57 +0545

    major changes

--- Admin/Included/sidebarmenuv2.php
+++ Admin/Included/sidebarmenuv2.php
@@ -7,21 +7,21 @@
             </div>
             <div class="img-admin d-flex flex-column align-items-center text-center gap-2">
                 <img class="rounded-circle" src="packageimages/yy.jpg" alt="img-admin" height="120" width="120">
-                <span class="h7 admin-color">admin</span>
+                <span class="h8 admin-color">Admin</span>
             </div>
             <div class=" bg-list d-flex flex-column align-items-center fw-bold gap-2 mt-4 ">
                 <ul class="d-flex flex-column list-unstyled">
-                        <li class="h7"><a class="nav-link text-dark" href="dashboard2.php"><span>Home</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="createpackage.php"> <span>Add Packages</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="managepackages.php"><span>Edit Packages</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="manageuser.php"><i<span>Manage Users</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="managebooking.php"><i<span>Manage bookings</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="manageissues.php"><span>Manage Issues</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="manageenquires.php"><span>Manage enquiries</span></a></li>
+                        <li class="h7"><a class="nav-link text-dark" href="dashboard2.php"><span class="colors">Home</span></a></li>
+                    <li class="h7"><a class=" nav-link text-dark" href="createpackage.php" > <span class="colors">Add Packages</span></a></li>
+                    <li class="h7"><a class=" nav-link text-dark" href="managepackages.php"><span class="colors">Edit Packages</span></a></li>
+                    <li class="h7"><a class=" nav-link text-dark" href="manageuser.php"><span class="colors">Manage Users</span></a></li>
+                    <li class="h7"><a class=" nav-link text-dark" href="managebooking.php"><span class="colors">Manage bookings</span></a></li>
+                    <li class="h7"><a class=" nav-link text-dark" href="manageissues.php"><span class="colors">Manage Issues</span></a></li>
+                    <li class="h7"><a class=" nav-link text-dark" href="manageenquires.php"><span class="colors">Manage enquiries</span></a></li>
                 </ul>
                 <ul class="logout d-flex justify-content-start list-unstyled">
-                    <li class=" h7"><a class="nav-link text-dark" href="index.php"><span>Logout</span><i
-                                class="fal fa-sign-out-alt ms-2"></i></a></li>
+                    <li class=" h7"><a class="nav-link text-dark" href="index.php"><span class="colors">Logout</span><i
+                                class="fal fa-sign-out-alt ms-2 colors"></i></a></li>
                 </ul>
             </div>
 
