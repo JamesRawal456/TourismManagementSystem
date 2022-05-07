72170e873c8d0cd64175a29d2c426ca53a033641
bipinsu <np03cs4s210247@heraldcollege.edu.np>
2022-05-07 13:57:57 +0545

    toll no Delete

--- includes/header.php
+++ includes/header.php
@@ -23,10 +23,9 @@
 			<li class="hm"><a href="index.php"><i class="fa fa-home"></i></a></li>
 				<li class="hm"><a href="admin/index.php">Admin Login</a></li>
 		</ul>
-		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
-			<li class="tol">Toll Number : 123-4568790</li>				
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 			
 			<li class="sig"><a href="#" data-toggle="modal" data-target="#myModal" >Sign Up</a></li> 
-			<li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" >/ Sign In</a></li>
+			<li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" > Sign In</a></li>
         </ul>
 		<div class="clearfix"></div>
 	</div>
@@ -38,12 +37,6 @@
 	<div class="container">
 		<div class="logo wow fadeInDown animated" data-wow-delay=".5s">
 			<a href="index.php">Tourism <span>Management System</span></a>	
-		</div>
-	
-		<div class="lock fadeInDown animated" data-wow-delay=".5s"> 
-			<li><i class="fa fa-lock"></i></li>
-            <li><div class="securetxt">SAFE &amp; SECURE </div></li>
-			<div class="clearfix"></div>
 		</div>
 		<div class="clearfix"></div>
 	</div>
